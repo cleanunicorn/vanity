d71600ea04b073504f6dbeb9dbacb17eb96ab1b9
Daniel Luca (@CleanUnicorn) <cleanunicorn@protonmail.com>
2021-10-30 10:06:19 +0000

    🎈 Add multithreading

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"crypto/ecdsa"
 	"encoding/hex"
 	"strings"
+	"time"
 
 	"github.com/ethereum/go-ethereum/crypto"
 )
@@ -19,17 +20,45 @@ func check_prefix(prefix string) *ecdsa.PrivateKey {
 	return nil
 }
 
+func check_prefix_routine(c chan *ecdsa.PrivateKey, prefix string) {
+	key, _ := crypto.GenerateKey()
+	addr := crypto.PubkeyToAddress(key.PublicKey)
+
+	if strings.HasPrefix(strings.ToLower(addr.Hex()), prefix) {
+		c <- key
+		return
+	}
+
+	c <- nil
+}
+
 func main() {
 
-	var key *ecdsa.PrivateKey
-	var prefix string = "0xdeadc0de"
+	var prefix string = "0xdeadcode"
+
+	c := make(chan *ecdsa.PrivateKey, 10)
+
+	for i := 0; i < 10; i++ {
+		go check_prefix_routine(c, prefix)
+	}
+
+	start := time.Now()
+	count := 0
 
 	for {
-		key = check_prefix(prefix)
+		count++
+		key := <-c
 		if key != nil {
+			print(hex.EncodeToString(key.D.Bytes()), "\n")
 			break
+		} else {
+			go check_prefix_routine(c, prefix)
+		}
+
+		elapsed := time.Now().Sub(start)
+		if (count%10000 == 0) && (int(elapsed.Seconds()) > 0) {
+			print("Speed: ", count/int(elapsed.Seconds()), " keys/sec \n")
 		}
 	}
 
-	print(hex.EncodeToString(key.D.Bytes()), "\n")
 }
